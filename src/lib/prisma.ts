4449a6c75c098228df15ce24df18c119197b824a
Skip McCormick <kinnicoun@gmail.com>
2026-06-09 11:57:25 +0200

    Configure Prisma 7 for Render

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,13 +1,11 @@
 import "dotenv/config";
 import { PrismaClient } from "../generated/prisma/client.js";
-// import { PrismaPg } from "@prisma/adapter-pg";
+import { PrismaPg } from "@prisma/adapter-pg";
 
-// const adapter = new PrismaPg({
-//     connectionString: process.env.DATABASE_URL!,
-// });
+const adapter = new PrismaPg({
+    connectionString: process.env.DATABASE_URL!,
+});
 
-// export const prisma = new PrismaClient({
-//     adapter,
-// });
-
-export const prisma = new PrismaClient();
+export const prisma = new PrismaClient({
+    adapter,
+});
